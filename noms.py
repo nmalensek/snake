bb63ccf5d51e3834d83dc97c67d3f311e21b5d84
nmalensek <nicholas@Nicholass-MacBook-Air.local>
2016-05-05 10:01:29 -0600

    init snake game
    added snake game folder

---
+++ noms.py
@@ -0,0 +1,70 @@
+import curses
+import time
+import random
+from snake import Snake
+
+# Initialize curses
+stdscr = curses.initscr()
+if curses.has_colors:
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_GREEN)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_RED)
+
+curses.curs_set(0)   # Don't display the text cursor
+curses.noecho()      # Don't echo key presses to the screen
+stdscr.keypad(True)  # Translate raw key codes into key identifiers
+stdscr.nodelay(True) # Non-blocking mode (don't pause and wait for keypresses)
+
+snake = Snake()
+maxyx = stdscr.getmaxyx()
+max_y = maxyx[0] - 1
+max_x = maxyx[1] - 1
+
+# f is for food, it's good enough for me
+stdscr.addstr(10, 15, 'f', curses.color_pair(2))
+stdscr.addstr(2, 2, 'f', curses.color_pair(2))
+stdscr.addstr(6, 8, 'f', curses.color_pair(2))
+
+score = 0
+
+
+while True:
+    snake.move_head()
+    if snake.x < 0 or snake.y < 0 or snake.x > max_x or snake.y > max_y:
+        # Out of bounds
+        break
+
+    headchar = chr(stdscr.inch(snake.y, snake.x) & 0xFF)
+    if headchar == 'x'
+        # Nommed self or border
+        break
+
+        # Draw new head position
+    stdscr.addstr(snake.y, snake.x, 'x', curses.color_pair(1))
+
+    if headchar == 'f':
+        add_food = stdscr.addstr(random.randint(0, max_y), random.randint(0, max_x), 'f', curses.color_pair(2))
+        add_food
+        score += 1
+        stdscr.addstr(0, 70, 'Score: %d' % score)
+        # Ate food! (add new food, update score)
+        pass
+    else:
+        # Didn't eat anything. Shrink the tail by putting a space at the end.
+        snake.move_tail()
+        stdscr.addstr(snake.tail_y, snake.tail_x, ' ')
+
+    # Apply all our graphics changes
+    stdscr.refresh()
+
+    time.sleep(0.05) # If I wanted to make the game harder, I might start here...
+
+    button = stdscr.getch()
+    if button == ord('q'):
+        # Q is for quitting, it's also good enough for me
+        break
+    else:
+        snake.set_direction(button)
+
+curses.endwin() # Out of the game loop, clean up curses
+print('bye!')
---
+++ snake.py
@@ -0,0 +1,40 @@
+from collections import deque
+import curses
+#class because OOP, design classes to encapsulate objects. Things that you want to put data in
+#are good classes. instead of having variables snake head, snake tail, etc. you just have class
+#easier to edit, easier to manipulate/expand (ex. multiplayer = 2 instances of snake class)
+class Snake:
+    # Maps keyboard keys to directions, based on y-x changes.
+    # For example, 'up' would move you in the negative y direction.
+    KEYMAP = {
+            # key            : (y, x)
+            curses.KEY_UP    : (-1, 0),
+            curses.KEY_DOWN  : (1, 0),
+            curses.KEY_LEFT  : (0, -1),
+            curses.KEY_RIGHT : (0, 1),
+    }
+    #start points, simpler
+    y = 5
+    x = 10
+    tail_y = 5
+    tail_x = 9
+    #initial direction, simple and quick design
+    body = deque([KEYMAP[curses.KEY_RIGHT]])
+    direction = KEYMAP[curses.KEY_RIGHT]
+
+    def set_direction(self, key):
+        try:
+            self.direction = self.KEYMAP[key]
+        except:
+            pass # This wasn't an arrow key
+
+    def move_head(self):
+        self.body.append(self.direction)
+        self.y += self.direction[0]
+        self.x += self.direction[1]
+
+    def move_tail(self):
+        tail_dir = self.body.popleft()
+        self.tail_y += tail_dir[0]
+        self.tail_x += tail_dir[1]
+
